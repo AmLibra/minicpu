ad3b4bd20fd1318e9d9412d9d6afe00b76ca40d4
Khalil <57001859+AmLibra@users.noreply.github.com>
2023-12-13 09:27:59 +0100

    Fix webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ module.exports = {
     mode: 'development',
     entry: './src/main.ts', // Change this to your main TypeScript file
     devtool: 'inline-source-map',
-    devServer: {
-        static: './dist'
-    },
     module: {
         rules: [
             {
@@ -22,7 +19,14 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8080,
+    },
 };
 
 
